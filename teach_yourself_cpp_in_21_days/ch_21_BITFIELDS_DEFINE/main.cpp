44cf002676258b9017b92f7761b8f8ff5e784752
alex-aleyan <sasha.aleyan@gmail.com>
2026-08-02 20:12:41 -0400

    fixed ch21

--- teach_yourself_cpp_in_21_days/ch_21_BITFIELDS_DEFINE/main.cpp
+++ teach_yourself_cpp_in_21_days/ch_21_BITFIELDS_DEFINE/main.cpp
@@ -1,7 +1,7 @@
 #define DEBUG
 #define SW_VERSION 5
 
-#define CUBE(A)		((A)*(A)*(A))
+#define CUBE(A)        ((A)*(A)*(A))
 #define BAD_CUBE(A) (A*A*A) 
 
 // #1.3 
@@ -17,29 +17,26 @@
 #define STRINGIZE(FREQ) ((#FREQ) )
 #endif
 
-//	#1.5
+//    #1.5
 
 #ifdef DEBUG
-	#define ASSERT(no_fault) \
-			if(no_fault == 0) \
-			{ \
-				cout << "FAULT OCCURED" << endl; \
-			} 
+    #define ASSERT(no_fault) \
+            if(no_fault == 0) \
+            { \
+                cout << "FAULT OCCURED" << endl; \
+            } 
 #endif
 
-//	#1.7
-#ifndef DEBUG
-	#define PRINT(x)
-#else
-#define PRINT(x) cout << #x ## " : " << x << endl;
-#endif
+#define PRINT(ptr) cout << *ptr << endl 
+#define PRINT_CONCAT_POINTERS(ptr1, ptr2) cout << *ptr1 << *ptr2 << endl 
+#define PRINT_CONCAT_STRINGS( str1, str2) cout <<  str1 <<  str2 << endl 
 
 #include <iostream>
 
 using std::endl;
 using std::cout;
 
-//	#3:
+//    #3:
 enum STATUS { PartTime, FullTime};
 enum GRADELEVEL { UnderGrad, Grad };
 enum HOUSING { OffCampus, Dorm  };
@@ -48,24 +45,24 @@ enum FOODPLAN { NoMeal, AllMeals };
 class StudentCT
 {
 public:
-	StudentCT():
-		itsStatus(FullTime),
-		itsGradLevel(UnderGrad),
-		itsHousing(Dorm),
-		itsFoodPlan(AllMeals)
-	{}
-	~StudentCT() { std::cout << "I'm done" << endl; };
-	STATUS getStatus() {if (itsStatus)	return FullTime; 
-						else			return PartTime; };
-	void setStatus(STATUS theStatus) { itsStatus = theStatus; };
-	unsigned int getPlan() { return itsFoodPlan; }
+    StudentCT():
+        itsStatus(FullTime),
+        itsGradLevel(UnderGrad),
+        itsHousing(Dorm),
+        itsFoodPlan(AllMeals)
+    {}
+    ~StudentCT() { std::cout << "I'm done" << endl; };
+    STATUS getStatus() {if (itsStatus)    return FullTime; 
+                        else            return PartTime; };
+    void setStatus(STATUS theStatus) { itsStatus = theStatus; };
+    unsigned int getPlan() { return itsFoodPlan; }
 
 
 private:
-	unsigned int itsStatus		:	1;
-	unsigned int itsGradLevel	:	1;
-	unsigned int itsHousing		:	1;
-	unsigned int itsFoodPlan	:	1;
+    unsigned int itsStatus        :    1;
+    unsigned int itsGradLevel    :    1;
+    unsigned int itsHousing        :    1;
+    unsigned int itsFoodPlan    :    1;
 };
 
 
@@ -74,56 +71,60 @@ int main()
 {
 
 // L21.1
-	cout << "Checking on the definitions of DEBUG, " << endl;
-	cout << "SW_version, and WINDOWS_VERSION; listing 21.1" << endl;
+    cout << "Checking on the definitions of DEBUG, " << endl;
+    cout << "SW_version, and WINDOWS_VERSION; listing 21.1" << endl;
 
-	#ifdef DEBUG
-		cout << "DEBUG is defined" << endl;
-	#else 
-		cout << "DEBUG is not defined" << endl;
-	#endif
+    #ifdef DEBUG
+        cout << "DEBUG is defined" << endl;
+    #else 
+        cout << "DEBUG is not defined" << endl;
+    #endif
 
-	#ifndef SW_VERSION
-		cout << "SW_VERSION is not defined" << endl;
-	#else
-		cout << "SW_VERSION is defined" << endl;
-	#endif
+    #ifndef SW_VERSION
+        cout << "SW_VERSION is not defined" << endl;
+    #else
+        cout << "SW_VERSION is defined" << endl;
+    #endif
 
-	#ifdef WINDOWS_VERSION
-		cout << "WINDOWS_VERSION is not defined" << endl;
-	#else
-		cout << "WINDOWS_VERSION is not defined" << endl;
-	#endif
+    #ifdef WINDOWS_VERSION
+        cout << "WINDOWS_VERSION is not defined" << endl;
+    #else
+        cout << "WINDOWS_VERSION is not defined" << endl;
+    #endif
 
 // L21.2:
-	int a = 1;
-	int b = 2;
-	int x = CUBE(a + b);
-	int y = BAD_CUBE(a + b);
-	int fault = 0;
+    int a = 1;
+    int b = 2;
+    int x = CUBE(a + b);
+    int y = BAD_CUBE(a + b);
+    int fault = 0;
 
-	cout << "CUBE: "	<< x << endl;
-	cout << "BAD_CUBE:" << y << endl;
+    cout << "CUBE: "    << x << endl;
+    cout << "BAD_CUBE:" << y << endl;
 
-	cout << STRINGIZE(Hey there) << endl;
+    cout << STRINGIZE(Hey there) << endl;
 
-	cout << "27 == x, true: " << x << endl;
-	ASSERT(27 == x); //		evaluates to true and no action is taken!
-	cout << "x == 5, false: " << x << endl;
-	ASSERT(x == 5); //		evaluates to false; action is taken!
+    cout << "27 == x, true: " << x << endl;
+    ASSERT(27 == x); //        evaluates to true and no action is taken!
+    cout << "x == 5, false: " << x << endl;
+    ASSERT(x == 5); //        evaluates to false; action is taken!
 
-	int *ptr = &a;
-	//PRINT(ptr); // FIXME: uncomment and resolve
-	//PRINT(*ptr);// FIXME: uncomment and resolve
+    std::string hello_w0r1d_str = "hello world!";
+    PRINT(&hello_w0r1d_str); // FIXME: uncomment and resolve
 
-//	#3:
-	StudentCT Jim;
+    std::string h311o_str = "h311o!";
+    std::string w0r1d_str = "w0r1d!";
+    PRINT_CONCAT_POINTERS(&h311o_str, &w0r1d_str); // pass sting's address
+    PRINT_CONCAT_STRINGS(  h311o_str,  w0r1d_str);    // pass string var
 
-	cout << "Jim.getPlan (1:All Meal, 2:No Meal):  " << Jim.getPlan() << endl;
-	cout << "Jim.getStatus (1:FullTime, 2:PartTime): " << Jim.getStatus() << endl;
+    //    #3:
+    StudentCT Jim;
 
-	cout << "Done." << endl;
-	system("sleep 1");
-	return 0;
+    cout << "Jim.getPlan (1:All Meal, 2:No Meal):  " << Jim.getPlan() << endl;
+    cout << "Jim.getStatus (1:FullTime, 2:PartTime): " << Jim.getStatus() << endl;
 
+    cout << "Done." << endl;
+    system("sleep 1");
+    return 0;
+    
 }
